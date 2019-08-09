0e9c8328f6153e27aca90136d71509f39bf70224
Ana Yankova <anayankova91@gmail.com>
2019-08-09 17:57:46 +0300

    add tasks

---
+++ Java OOP/l_Exam/High Quality Structure/Main.java
@@ -0,0 +1,21 @@
+import core.EngineImpl;
+import core.interfaces.Engine;
+import models.players.Advanced;
+import models.players.Beginner;
+import models.players.interfaces.Player;
+import repositories.CardRepositoryImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello World!");
+
+        //Player beginner = new Advanced(new CardRepositoryImpl(), "username");
+        //boolean test = beginner instanceof Beginner;
+
+        //System.out.println(test);  //false
+
+        Engine engine = new EngineImpl();
+        engine.run();
+    }
+}
